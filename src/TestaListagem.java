28874271686325aea241d3070ca836a246e97f28
Patricia <patyamiga16@hotmail.com>
2021-12-15 10:28:25 -0300

    teste

--- src/TestaListagem.java
+++ src/TestaListagem.java
@@ -1,33 +1,30 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.mysql.cj.xdevapi.Result;
-
 public class TestaListagem {
 
 	public static void main(String[] args) throws SQLException {
-		
-		Connection connection = DriverManager.getConnection
-				("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "1234");
-		
-			Statement stm = connection.createStatement();
-			stm.execute("Select ID, Nome, Descricao from Produto");
-			
-			ResultSet rst = stm.getResultSet();
-				
-				while(rst.next()) {
-					Integer id = rst.getInt("id");
-					System.out.println(id);
-					String nome = rst.getString("nome");
-					System.out.println(nome);
-					String descricao = rst.getString("descricao");
-					System.out.println(descricao);
 
-				}					
-			connection.close(); 
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+
+		Statement stm = connection.createStatement();
+		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+
+		ResultSet rst = stm.getResultSet();
+
+		while (rst.next()) {
+			Integer id = rst.getInt("ID");
+			String nome = rst.getString("NOME");
+			String descricao = rst.getString("DESCRICAO");
+
+			System.out.println(nome);
+			System.out.println(id);
+			System.out.println(descricao);
+
+		}
+		connection.close();
 	}
 }
-
