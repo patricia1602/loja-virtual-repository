2204054459be9ed88d6ec035475e0a0aaed68656
Patricia <patyamiga16@hotmail.com>
2021-12-16 12:17:30 -0300

    teste2

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -8,18 +8,38 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "Mouse";
-		String descricao = "Mouse sem fio); delete from Produto";
-
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
 
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES(?,?)",
-				Statement.RETURN_GENERATED_KEYS);
+		connection.setAutoCommit(false);
+
+		try {
+			PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES(?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			adicionarVariavel("Smart TV", "45 polegadas", stm);
+			adicionarVariavel("Radio", "Radio de bateria", stm);
+
+			connection.commit();
+
+			stm.close();
+			connection.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ROLLBACK EXECUTADO");
+			connection.rollback();
+		}
+}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
 
+		
+		 if (nome.equals("Radio")) { throw new
+		 RuntimeException("Não foi possível adicionar o produto"); }
+		 
 		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
@@ -28,5 +48,7 @@ public class TestaInsercaoComParametro {
 			System.out.println("O id criado foi: " + id);
 
 		}
+		rst.close();
+	    }
 	}
-}
+
